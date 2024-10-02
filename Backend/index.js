49ca396681da3e62cc872ef12f64a681e08a4f8b
Avik Roy <somavik99@gmail.com>
2024-10-02 10:39:09 +0530

    mongodb connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,46 @@
-import express from "express"
-import dotenv from "dotenv"
-import mongoose from "mongoose"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
+dotenv.config();
 
+const app = express();
 
-dotenv.config()
+const PORT = process.env.PORT || 8000;
 
-const app = express()
+mongoose.set("strictQuery", false);
 
+async function mongooseConnection() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("DB connected successfully");
+  } catch (err) {
+    console.log("Error happened: " + err.message);
+  }
+}
 
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      return callback(null, true);
+    },
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+
+mongooseConnection()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is listening to the port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`Error in connecting : ${error.message}`);
+  });
